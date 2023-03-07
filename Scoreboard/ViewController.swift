5d71cbbe9e3a0cf07ca0a71ccd4114f0e32fc6f3
shunsuke kawata <kwt20238120@gmail.com>
2023-03-07 15:12:50 +0900

    create 作成していたUIの作成

---
+++ Scoreboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scoreboard
+//
+//  Created by 川田　隼輔 on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
