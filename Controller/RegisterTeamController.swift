459fcc71f364d879c954d001ed88e460dcdb10c0
shunsuke kawata <kwt20238120@gmail.com>
2023-03-14 06:03:04 +0900

    create 画面の作成と画面遷移メソッドの理解

---
+++ Controller/RegisterTeamController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterTeamController.swift
+//  Scoreboard
+//
+//  Created by 川田　隼輔 on 2023/03/14.
+//
+
+
+
+//Mainを操作するcontrollerの役割
+import UIKit
+
+class RegisterTeamController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- Scoreboard/ViewController.swift
+++ Scoreboard/ViewController.swift
@@ -5,13 +5,34 @@
 //  Created by 川田　隼輔 on 2022/11/17.
 //
 
+
+//Mainを操作するcontrollerの役割
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toRecordButton: UIButton!
+    
+    @IBOutlet weak var toRegisterTeamButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func transitionToRegisterTeam(_ sender : Any){
+        let storyboard = UIStoryboard(name: "RegisterTeam", bundle: nil)
+        
+                let registerTeamController = storyboard.instantiateViewController(withIdentifier: "RegisterTeamController")
+        
+                //navigationControllerクラスがない場合はメソッドそのものが呼び出されない
+                self.navigationController?.pushViewController(registerTeamController, animated: true)
+        
+    }
+    @IBAction func transitionToRecord(_ sender : Any){
+        print("record")
+        
     }
     
 
