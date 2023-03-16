88942a2bcda73886c6d30c78a5f4f852eb3c6bd2
shunsuke kawata <kwt20238120@gmail.com>
2023-03-17 03:52:31 +0900

    update UIViewからの入力処理を実装

--- Controller/RegisterNewTeamController.swift
+++ Controller/RegisterNewTeamController.swift
@@ -5,24 +5,35 @@
 //  Created by 川田　隼輔 on 2023/03/15.
 //
 import UIKit
-import Eureka
 
 class RegisterNewTeamController:UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
     //最大で26人分の選手データを登録する配列
     var regiserTeamData = [
-        ["1",""],["2",""],["3",""],["4",""],["5",""],["6",""],["7",""],["8",""],
-        ["9",""],["10",""],["11",""],["12",""],["13",""],["14",""],["15",""],["16",""],
-        ["17",""],["18",""],["19",""],["20",""],["21",""],["22",""],["23",""],["24",""],
+        ["1",""],["2",""],
+        ["3",""],["4",""],
+        ["5",""],["6",""],
+        ["7",""],["8",""],
+        ["9",""],["10",""],
+        ["11",""],["12",""],
+        ["13",""],["14",""],
+        ["15",""],["16",""],
+        ["17",""],["18",""],
+        ["19",""],["20",""],
+        ["21",""],["22",""],
+        ["23",""],["24",""],
         ["25",""],["26",""]
     ]
-    let selectNumber: [Int] = Array(1...99)
+
     
     @IBOutlet weak var backButton: UIButton!
-    //tableViewの作成
+    @IBOutlet weak var registerSubmitButton: UIButton!
+    
+    @IBOutlet weak var registerFormTableView: UITableView!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return 26;
         }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          // セルを取得する
          let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "proto", for: indexPath)
@@ -34,16 +45,43 @@ class RegisterNewTeamController:UIViewController, UITableViewDelegate, UITableVi
          // Labelにテキストを設定する
          numberField.text = regiserTeamData[indexPath.row][0]
          nameField.text = regiserTeamData[indexPath.row][1]
+        
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+          print("変更あり")
+          return true
+        }
+        
+        
 
          return cell
      }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         }
     
+    //戻るボタンをタップしたとき
     @IBAction func backButtonTapped(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    //登録ボタンをタップしたとき
+    @IBAction func registerSubmitButtonTapped(_ sender: UIButton) {
+        print("clicked")
+    }
+    
+    
+    //TableViewからformで送信して登録する値を取得してくる
+    
+    @IBAction func changePlayerNumber(_ sender: UITextField) {
+        let changedNumber = String(sender.text!)
+            print(changedNumber)
+    }
+    @IBAction func changePlayerName(_ sender: UITextField) {
+        let changedName = String(sender.text!)
+            print(changedName)
+    }
+    
 }
 
 
