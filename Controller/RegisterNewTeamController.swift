d67413f757a28796e3796e099e2459be6563a9ba
shunsuke kawata <kwt20238120@gmail.com>
2023-03-17 10:50:35 +0900

    fix string型のネスト配列からdictionary型の配列に変更

--- Controller/RegisterNewTeamController.swift
+++ Controller/RegisterNewTeamController.swift
@@ -9,20 +9,34 @@ import UIKit
 class RegisterNewTeamController:UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
     //最大で26人分の選手データを登録する配列
-    var registerTeamData:[[String]] = [
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""],
-        ["",""],["",""]
+    //dict型の配列に変更予定
+    var registerTeamData:[Dictionary<String,String>] = [
+        ["number":"1","name":""],
+        ["number":"2","name":""],
+        ["number":"3","name":""],
+        ["number":"4","name":""],
+        ["number":"5","name":""],
+        ["number":"6","name":""],
+        ["number":"7","name":""],
+        ["number":"8","name":""],
+        ["number":"9","name":""],
+        ["number":"10","name":""],
+        ["number":"11","name":""],
+        ["number":"12","name":""],
+        ["number":"13","name":""],
+        ["number":"14","name":""],
+        ["number":"15","name":""],
+        ["number":"16","name":""],
+        ["number":"17","name":""],
+        ["number":"18","name":""],
+        ["number":"19","name":""],
+        ["number":"20","name":""],
+        ["number":"21","name":""],
+        ["number":"22","name":""],
+        ["number":"23","name":""],
+        ["number":"24","name":""],
+        ["number":"25","name":""],
+        ["number":"26","name":""]
     ]
 
     @IBOutlet weak var backButton: UIButton!
@@ -44,11 +58,12 @@ class RegisterNewTeamController:UIViewController, UITableViewDelegate, UITableVi
         let indexlabel = cell.contentView.viewWithTag(1) as!UILabel
         indexlabel.text = String(indexPath.row+1)
         
+        let row = registerTeamData[indexPath.row]
         let numberField = cell.contentView.viewWithTag(2) as!UITextField
-        numberField.text = registerTeamData[indexPath.row][0]
+        numberField.text = row["number"]!
 
         let nameField = cell.contentView.viewWithTag(3) as! UITextField
-        nameField.text = registerTeamData[indexPath.row][1]
+        nameField.text = row["name"]!
         
          return cell
      }
@@ -72,10 +87,24 @@ class RegisterNewTeamController:UIViewController, UITableViewDelegate, UITableVi
     //TableViewからformで送信して登録する値を取得してくる
     
     //配列の内容を更新する関数
-    func updateRegisterTeamData(index:Int,value:String,option:Int){
+    func updateRegisterTeamData(index:Int,value:String,option:String){
         registerTeamData[index-1][option] = value
+        let _ = validateRegisterData()
         print(registerTeamData)
         return
+    }
+    
+    func validateRegisterData()->Bool{
+//        for datum in registerTeamData{
+//            //登録しないデータ　正常系としてcontinueで飛ばす
+//            if(datum["number"]=="" && datum["name"]==""){
+//                continue
+//            }
+//            
+//            if(datum["num"])
+//            
+//        }
+        return true
     }
     //Playerの背番号を更新する関数
     @IBAction func changePlayerNumber(_ sender: UITextField) {
@@ -83,7 +112,7 @@ class RegisterNewTeamController:UIViewController, UITableViewDelegate, UITableVi
         if let changedSuperview  = sender.superview{
             let index = changedSuperview.viewWithTag(1) as!UILabel
             if let indexInt = Int(index.text!) {
-                updateRegisterTeamData(index: indexInt, value: changedNumber, option: 0)
+                updateRegisterTeamData(index: indexInt, value: changedNumber, option: "number")
             } else {
                 print("数値ではありません。")
                 return
@@ -99,7 +128,7 @@ class RegisterNewTeamController:UIViewController, UITableViewDelegate, UITableVi
         if let changedSuperview  = sender.superview{
             let index = changedSuperview.viewWithTag(1) as!UILabel
             if let indexInt = Int(index.text!) {
-                updateRegisterTeamData(index: indexInt, value: changedName, option: 1)
+                updateRegisterTeamData(index: indexInt, value: changedName, option: "name")
             } else {
                 print("数値ではありません。")
                 return
--- Model/registerNewTeamModel.swift
+++ Model/registerNewTeamModel.swift
@@ -10,22 +10,13 @@ import FirebaseFirestore
 
 class registerNewTeamModel{
     
-    func registerNewTeam(teamName:String,members:[[String]]) {
+    func registerNewTeam(teamName:String, members:[Dictionary<String,String>]) {
         
-        var dataToAdd:[Dictionary<String,Any>] = []
-        
-        members.forEach{
-            member in
-            let tmp:[String:Any] = [
-                "number" :member[0],
-                "name":member[1]
-            ]
-            dataToAdd.append(tmp)
-        }
+
         let db = Firestore.firestore()
         let documentRef = db.collection("teams").document()
         
-        let teamInfomationToRegister = ["teamName": teamName, "members": dataToAdd] as [String: Any]
+        let teamInfomationToRegister = ["teamName": teamName, "members": members] as [String: Any]
 
         documentRef.setData(teamInfomationToRegister) { error in
             if let error = error {
