44048bb5a1a8599cdc561eddb7a77e90e46d6fed
川田隼輔 <kwt20238120@gmail.com>
2023-08-11 19:34:30 +0900

    udpate 試合情報を登録する画面の見た目を完成

--- Controller/RegisterNewGameController.swift
+++ Controller/RegisterNewGameController.swift
@@ -9,8 +9,6 @@ import Foundation
 import UIKit
 import RealmSwift
 
-let weather:Dictionary<String,String> = ["晴れ":"sunny","くもり":"cloudy","雨":"rainy"]
-
 enum weatherType: CaseIterable {
     case sunny
     case cloudy
@@ -29,11 +27,11 @@ enum weatherType: CaseIterable {
 }
 
 class RegisterNewGameController:UIViewController{
+    let weather:Dictionary<String,String> = ["晴れ":"sunny","くもり":"cloudy","雨":"rainy"]
+    var teamsArray:[String] = []
     
     let showInstance = ShowTeamsModel()
-    
-    
-    
+    var teams: Results<Team>!
     
     @IBOutlet weak var backButton: UIButton!
     
@@ -48,10 +46,46 @@ class RegisterNewGameController:UIViewController{
     
     //天気の初期値
     private var selectedWeather = weatherType.sunny
+    private var selectedMyteam:String?
+    private var selectedYourteam:String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //データベースからのデータ取得（初回以降）
+        teams = showInstance.fetchAllTeamsData()
+        
+        // データの取得後の処理を実行する
+        if let teams = teams {
+            teamsArray=[]
+            for team in teams {
+                teamsArray.append(team.name)
+            }
+            
+            if (teamsArray.count != 0){
+                selectedMyteam = teamsArray[0]
+                selectedYourteam = teamsArray[0]
+            }else{
+                selectedMyteam = "--"
+                selectedYourteam = "--"
+            }
+        }
+        
+        //それぞれのボタンのメニューを設定する
+        configureWeatherMenu()
+        configureMyTeamMenu(teamsArray: teamsArray)
+        configureYourTeamMenu(teamsArray: teamsArray)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        teams = showInstance.fetchAllTeamsData()
+        
+        //それぞれのボタンのメニューを設定する
         configureWeatherMenu()
+        configureMyTeamMenu(teamsArray:teamsArray)
+        configureYourTeamMenu(teamsArray: teamsArray)
+        
     }
     @IBAction func backButtonTapped(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
@@ -59,12 +93,12 @@ class RegisterNewGameController:UIViewController{
     
     private func configureWeatherMenu(){
         let actions = weatherType.allCases
-            .compactMap { type in
+            .compactMap { i in
                 UIAction(
-                    title:type.title,
-                    state:type == selectedWeather ? .on : .off,
+                    title:i.title,
+                    state:i == selectedWeather ? .on : .off,
                     handler: { _ in
-                        self.selectedWeather = type
+                        self.selectedWeather = i
                         self.configureWeatherMenu()
                     }
                 )
@@ -72,5 +106,37 @@ class RegisterNewGameController:UIViewController{
         wetherSelectButton.menu = UIMenu(title: "", options: .displayInline, children: actions)
         wetherSelectButton.showsMenuAsPrimaryAction = true
         wetherSelectButton.setTitle(selectedWeather.title, for: .normal)
+    }
+    
+    private func configureMyTeamMenu(teamsArray:[String]){
+        let actions = teamsArray.compactMap{ i in
+                UIAction(
+                    title:i,
+                    state:i == selectedMyteam ? .on : .off,
+                    handler: { _ in
+                        self.selectedMyteam = i
+                        self.configureMyTeamMenu(teamsArray:teamsArray)
+                    }
+                )
+        }
+        myTeamSelectButton.menu = UIMenu(title: "", options: .displayInline, children: actions)
+        myTeamSelectButton.showsMenuAsPrimaryAction = true
+        myTeamSelectButton.setTitle(selectedMyteam, for: .normal)
+    }
+    
+    private func configureYourTeamMenu(teamsArray:[String]){
+        let actions = teamsArray.compactMap{ i in
+                UIAction(
+                    title:i,
+                    state:i == selectedYourteam ? .on : .off,
+                    handler: { _ in
+                        self.selectedYourteam = i
+                        self.configureYourTeamMenu(teamsArray:teamsArray)
+                    }
+                )
+        }
+        yourTeamSelectButton.menu = UIMenu(title: "", options: .displayInline, children: actions)
+        yourTeamSelectButton.showsMenuAsPrimaryAction = true
+        yourTeamSelectButton.setTitle(selectedYourteam, for: .normal)
     }
 }
